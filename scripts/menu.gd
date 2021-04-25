82d2fe6e81b749a9a604713e2905894cd3971e55
Christian Wieprecht <christian.wieprecht@udo.edu>
2021-04-25 19:30:13 +0200

    Improved controls toggle.

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -6,3 +6,10 @@ func _on_play_pressed():
 
 func _on_mouse_controls_toggled(button_pressed):
 	Globals.mouse_controls = button_pressed
+
+func _on_mouse_controls_pressed():
+	Globals.mouse_controls = !Globals.mouse_controls
+	if Globals.mouse_controls:
+		$mouse_controls.text = "Right Click"
+	else:
+		$mouse_controls.text = "WASD"
