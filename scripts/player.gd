ab46d2ca8d6274af619a4dd75575259860b59927
Christian Wieprecht <christian.wieprecht@udo.edu>
2021-04-25 15:09:26 +0200

    Improved player animation and movement.

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,4 +8,11 @@ func _physics_process(delta):
 		target_pos = get_global_mouse_position()
 	if global_position.distance_to(target_pos) > 5:
 		var direction = (target_pos - global_position).normalized()
+		look_at(target_pos)
+		$worm.play("swim")
+		$worm.rotation_degrees = 90
 		move_and_slide(direction * speed)
+	else:
+		rotation_degrees = 0
+		$worm.rotation_degrees = 0
+		$worm.play("idle")
